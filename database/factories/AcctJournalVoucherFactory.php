ccd1a6bcba9de415189a3f6ae531ec03bd5212d5
verapera <116628678+verapera@users.noreply.github.com>
2024-01-18 13:38:48 +0700

    Jurnal voucher

--- database/factories/AcctJournalVoucherFactory.php
+++ database/factories/AcctJournalVoucherFactory.php
@@ -47,25 +47,6 @@ class AcctJournalVoucherFactory extends Factory
             'deleted_id' => null, 
         ];
         
-        // return [
-        //     'journal_voucher_status' => $faker->numberBetween(0, 1),
-        //     'transaction_module_code' => $faker->word,
-        //     'transaction_journal_id' => $faker->numberBetween(1, 100),
-        //     'transaction_journal_no' => $faker->word,
-        //     'journal_voucher_no' => $faker->word,
-        //     'journal_voucher_period' => $faker->word,
-        //     'journal_voucher_date' => $faker->date(),
-        //     'journal_voucher_title' => $faker->sentence,
-        //     'journal_voucher_description' => $faker->paragraph,
-        //     'journal_voucher_token' => $faker->word,
-        //     'reverse_state' => $faker->boolean,
-        //     'pickup_status' => $faker->boolean,
-        //     'created_id' => $faker->numberBetween(1, 10),
-        //     'updated_id' => $faker->numberBetween(1, 10),
-        //     'deleted_id' => null,
-        //     'created_at' => $faker->dateTimeThisMonth,
-        //     'updated_at' => $faker->dateTimeThisMonth,
-        //     'deleted_at' => null,
-        // ];
+       
     }
 }
--- database/factories/AcctJournalVoucherItemFactory.php
+++ database/factories/AcctJournalVoucherItemFactory.php
@@ -32,21 +32,6 @@ class AcctJournalVoucherItemFactory extends Factory
             'deleted_id' => null, 
         ];
         
-        // return [
-        //     'journal_voucher_id' => null,
-        //     'account_id' => null,
-        //     'journal_voucher_description' => $this->faker->sentence,
-        //     'journal_voucher_amount' => $this->faker->randomFloat(2, 1, 1000),
-        //     'account_id_status' => $this->faker->boolean,
-        //     'account_id_default_status' => $this->faker->boolean,
-        //     'journal_voucher_debit_amount' => $this->faker->randomFloat(2, 1, 1000),
-        //     'journal_voucher_credit_amount' => $this->faker->randomFloat(2, 1, 1000),
-        //     'reverse_state' => $this->faker->boolean,
-        //     'created_id' => null,
-        //     'updated_id' => null,
-        //     'deleted_id' => null,
-        //     'created_at' => now(),
-        //     'updated_at' => now(),
-        // ];
+       
     }
 }
